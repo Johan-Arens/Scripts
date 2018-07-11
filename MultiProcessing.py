1f570a50bdd2b5d27f52c9cb451d16fa64052065
Johan Arens <johan.arens@gmail.com>
2018-07-11 13:44:54 -0400

    ajouté commentaires

--- MultiProcessing.py
+++ MultiProcessing.py
@@ -1,18 +1,19 @@
+#
+#  Johan Arens
+#
+
 import time
 import multiprocessing
 def worker(id):
     for number in range(10):
-      print "looping number : " + str(number) + " - worker id : " + str(id)
-      if id == 5:
-        print "id 5 is slower"
-        time.sleep(3)
-      else:
-        time.sleep(0.1)
+        print "looping number : " + str(number) + " - worker id : " + str(id)
+        print "id "+ str(id) +" waits for " + str(id) + " sec"
+        time.sleep(id/10)
     return
 
 if __name__ == '__main__':
     jobs = []
-    for i in range(6):
+    for i in range(1000):
         p = multiprocessing.Process(target=worker, args=(i,))
         jobs.append(p)
         p.start()
